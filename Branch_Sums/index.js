7ee173030ab6f9e92390bdc66629910a26dd446a
Kalina Georgieva <kalina.georgieva@tick42.com>
2022-08-20 21:53:01 +0300

    Solve Branch Sums task
    Signed-off-by: Kalina Georgieva <kalina.georgieva@tick42.com>

---
+++ Branch_Sums/index.js
@@ -0,0 +1,44 @@
+const branchSums = (root) => {
+    const allSumsArray = [];
+    const initialBranchSum = 0;
+
+    sumTreeBranches(root, initialBranchSum, allSumsArray);
+
+    return allSumsArray;
+};
+
+const sumTreeBranches = (node, currentBranchSum, allSums) => {
+    if (!node) {
+        return;
+    }
+
+    currentBranchSum += node.value;
+
+    if (!node.left && !node.right) {
+        allSums.push(currentBranchSum);
+    }
+
+    sumTreeBranches(node.left, currentBranchSum, allSums);
+    sumTreeBranches(node.right, currentBranchSum, allSums);
+};
+
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+const root = new Node(1);
+root.left = new Node(2);
+root.right = new Node(3);
+root.left.left = new Node(4);
+root.left.right = new Node(5);
+root.right.left = new Node(6);
+root.right.right = new Node(7);
+root.left.left.left = new Node(8);
+root.left.left.right = new Node(9);
+root.left.right.left = new Node(10);
+
+console.log(branchSums(root));
